150f1f6d98e1e8f4d34dddf3084d07902d5cb111
godotto <m.marcinkiewicz@protonmail.com>
2022-12-15 18:38:11 +0100

    Init adaptive Huffman coding class

---
+++ src/adaptive_huffman.py
@@ -0,0 +1,14 @@
+import node
+
+NYT = "NYT"
+
+class AdaptiveHuffmanEncoderDecoder:
+    def __init__(self, input_data: bytes, alphabet_size: int):
+        self.input_data = input_data
+        self.alphabet_size = alphabet_size
+
+        self.e = alphabet_size.bit_length() - 1
+        self.r = alphabet_size - 2 ** self.e
+
+        self.root = node.Node(0, NYT)
+    
