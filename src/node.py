473a5e670148a5a8fe5e514b159fb9352053713e
godotto <m.marcinkiewicz@protonmail.com>
2022-12-15 18:36:27 +0100

    Node object for tree creation

---
+++ src/node.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, weight: int, symbol):
+        self.left = None
+        self.right = None
+        self.parent = None
+
+        self.symbol = symbol
+        self.weight = weight
+        self.code = "" # for printing purpose
