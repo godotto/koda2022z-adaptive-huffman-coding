12d00e03efcbc16ac20185067c07b5df6e49503a
Ruadhri17 <k.r.piotrowski17@gmail.com>
2022-12-16 03:08:24 +0100

    partial tree update solution

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ from node import Node
 
 NYT = "NYT"
 
+
 def switch_nodes(left_node: Node, right_node: Node):
     # switch references to the left node
     left_node.left, right_node.left = right_node.left, left_node.left
@@ -21,7 +22,8 @@ def print_code(root: Node, symbol):
         current_node = nodes_to_check.pop()
 
         if current_node.symbol == symbol:
-            return (current_node.code, False) # returns code and false if it is not a new symbol on a tree
+            # returns code and false if it is not a new symbol on a tree
+            return (current_node.code, False)
         if current_node.symbol == NYT:
             code_for_new_symbol = current_node.code
         if current_node.left:
@@ -30,5 +32,60 @@ def print_code(root: Node, symbol):
         if current_node.right:
             current_node.right.code = current_node.code + '1'
             nodes_to_check.append(current_node.right)
-        
-    return (current_node.code, True) # returns code for a new symbok and true if it is a new symbol on a tree
+
+    # returns code for a new symbok and true if it is a new symbol on a tree
+    return (current_node.code, True)
+
+
+def update(root: Node, symbol, first_apperance):
+    current_node = None
+    while True:
+        if first_apperance:
+            current_node = find_node_symbol(root, NYT)
+            current_node.right = Node(1, symbol)
+            current_node.left = Node(0, NYT)
+            current_node.weight += 1
+            current_node.symbol = None
+            current_node.left.parent = current_node
+        else:
+            if not current_node:
+                current_node = find_node_symbol(root, symbol)
+
+    return
+
+
+def find_node_symbol(root: Node, symbol) -> Node:
+
+    if root.symbol == symbol:
+        return root
+
+    if root.left != None:
+        node = find_node_symbol(root.left, symbol)
+        if node != None:
+            return node
+
+    if root.right != None:
+        node = find_node_symbol(root.right, symbol)
+        if node != None:
+            return node
+
+    return None
+# te funkcje można połączyć w jedno ^v
+
+
+def find_node_weight(root: Node, weight) -> Node:
+
+    if root.weight == weight:
+        return root
+
+    if root.left != None:
+        node = find_node_weight(root.left, weight)
+        if node != None:
+            return node
+
+    if root.right != None:
+        node = find_node_weight(root.right, weight)
+        if node != None:
+            return node
+
+    return None
