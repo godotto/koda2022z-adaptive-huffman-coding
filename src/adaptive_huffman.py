8bfe10be8adc6b7bc68a7fd608c45b0be5fdbc48
Faustyna Brańko <f.branko@stud.elka.pw.edu.pl>
2022-12-19 16:17:04 +0100

    added fixed alphabet

--- src/adaptive_huffman.py
+++ src/adaptive_huffman.py
@@ -1,6 +1,6 @@
 import node
 import utils
-
+import string
 
 NYT = "NYT"
 
@@ -15,6 +15,22 @@ class AdaptiveHuffmanEncoderDecoder:
         self.root = node.Node(0, NYT, alphabet_size * 2 - 1)
 
         self.nodes_list = [self.root]
+        print("e: " + str(self.e))
+        print("r: " + str(self.r))
+
+        # used alphabet
+        alphabet = list(string.ascii_lowercase)
+        # generating fixed code for udes alphabet
+
+        self.fixed_code = []
+        for i in range(len(alphabet)):
+            if 0 < i < 2 * self.r - 1:
+                bin_a = '{0:b}'.format(i)
+                bin_a = bin_a.zfill(self.e + 1)
+            else:
+                bin_a = '{0:b}'.format(i - self.r)
+                bin_a = bin_a.zfill(self.e)
+            self.fixed_code.append(bin_a)
 
     def encode(self, input_data: bytearray):
         code = ""
@@ -25,6 +41,7 @@ class AdaptiveHuffmanEncoderDecoder:
             print(symbol_code + " ")
             code += symbol_code
             utils.update(self.root, input_data[i], self.nodes_list)
+        print(code)
         utils.add_padding(code)
         byte_array = utils.convert_to_bytes(code)
         utils.write_to_file(byte_array)
