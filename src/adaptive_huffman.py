699cfb2a4d2430e1506a77d0bc3051807d1e7bd8
Ruadhri17 <k.r.piotrowski17@gmail.com>
2022-12-20 01:25:02 +0100

    finished decode

--- src/adaptive_huffman.py
+++ src/adaptive_huffman.py
@@ -14,7 +14,6 @@ class AdaptiveHuffmanEncoderDecoder:
         self.r = alphabet_size - 2 ** self.e
 
         self.root = node.Node(0, NYT, alphabet_size * 2 - 1)
-
         self.nodes_list = [self.root]
         print("e: " + str(self.e))
         print("r: " + str(self.r))
@@ -46,3 +45,35 @@ class AdaptiveHuffmanEncoderDecoder:
         utils.add_padding(code)
         byte_array = utils.convert_to_bytes(code)
         utils.write_to_file(byte_array)
+
+    def decode(self, input: str):
+        i = 0  # current bit from the string
+        bits = ""  # current bits input
+        decoded = ""  # decoded sequence
+        current_node = self.root  # begin with root
+        while i < len(input) - 1:
+            if current_node.symbol:
+                if current_node.symbol == NYT:
+                    if i != 0:
+                        i += 1
+                    bits += input[i:i+self.e]
+                    i += self.e - 1
+                    if int(bits, 2) < self.r:
+                        i += 1
+                        bits += input[i]
+                    else:
+                        bits = format(int(bits, 2) + self.r, 'b')
+                    letter = self.alphabet[int(bits, 2)]
+                else:
+                    letter = self.alphabet[int(bits, 2) - 1]
+                decoded += letter
+                utils.update(self.root, letter, self.nodes_list)
+                current_node = self.root
+                bits = ""
+            else:
+                i += 1
+                bit = input[i]
+                current_node = current_node.right if int(
+                    bit) else current_node.left
+                bits += bit
+        print(decoded)
